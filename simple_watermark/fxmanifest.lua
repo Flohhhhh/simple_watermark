6a99cc70441c7b3696531dbc8f0cf9600b881c86
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2021-04-29 16:10:14 -0400

    Add files via upload

---
+++ simple_watermark/fxmanifest.lua
@@ -0,0 +1,22 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Floh'
+description 'Simple watermark resource for FiveM servers with smooth transitions, and easy configuration.'
+version '1.0.0'
+
+
+
+ui_page "ui/ui.html"
+
+files {
+  'ui/ui.js',
+  'ui/ui.css',
+  'ui/logo.png',
+  'ui/ui.html',
+}
+
+client_scripts {
+    'client.lua',
+}
